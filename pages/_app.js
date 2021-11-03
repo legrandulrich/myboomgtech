ef3880338dd5d3c6c67aa417110733e04c495968
Rich40 <legrandulrich@gmail.com>
2021-11-03 04:23:29 +0100

    start update dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -78,7 +78,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>NextJS Argon Dashboard by Creative Tim</title>
+          <title>BoomGTech</title>
           <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
         </Head>
         <Layout>
--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,19 +1,3 @@
-/*!
-
-=========================================================
-* NextJS Argon Dashboard PRO - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-argon-dashboard-pro
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 // node.js library that concatenates classes (strings)
 import classnames from "classnames";
@@ -73,7 +57,7 @@ function Dashboard() {
   }
   return (
     <>
-      <CardsHeader name="Default" parentName="Dashboards" />
+      // <CardsHeader name="Default" parentName="Dashboards" />
       <Container className="mt--6" fluid>
         <Row>
           <Col xl="8">
